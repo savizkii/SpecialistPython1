55f883204661d68b20371bc80dc8faf88acd0bbc
savizkii <106909895+savizkii@users.noreply.github.com>
2022-06-26 11:56:56 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -2,8 +2,10 @@
 # Функция должна вызываться с любым количеством аргументов
 
 def average(*args):
-    # TODO: your code here
-    pass
+    summ_elements = 0
+    for el in args:
+        summ_elements = summ_elements + el
+    return summ_elements / len(args)
 
 
 print(average(3, 4, 8))
